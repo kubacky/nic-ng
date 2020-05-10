a0b8ca3277b96e316b12a90512ea558c48ed52b9
kubacky <kubacki.artur@gmail.com>
2020-05-10 17:01:54 +0200

    add ant design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pl_PL } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pl from '@angular/common/locales/pl';
+
+registerLocaleData(pl);
 
 @NgModule({
   declarations: [
@@ -10,9 +19,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: pl_PL }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
